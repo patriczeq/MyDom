cb545e916e2b57e6925e778fb93a4db60ea55556
Patrik Eder <48981056+patriczeq@users.noreply.github.com>
2020-01-13 23:09:00 +0100

    Update mydom.js

--- mydom.js
+++ mydom.js
@@ -294,16 +294,16 @@
             }
           };
           xhttp.onload = function() {
-            var status = lib._xhr_.sts[Number(xhttp.status)],
+            var status = String(xhttp.status) in lib._xhr_.sts ? lib._xhr_.sts[xhttp.status] : "unknown",
               co_sts = Math.round(Number(xhttp.status) / 100);
-            if ([2, 3].indexOf(co_sts) !== -1 || !(Number(xhttp.status) in lib._xhr_.sts)) {
+            if ([2, 3].indexOf(co_sts) !== -1 || !(String(xhttp.status) in lib._xhr_.sts)) {
               return "function" === typeof success ?
                 success(lib._xhr_.parse ? lib.response_parser(xhttp.response, forceParse) : xhttp.response) :
                 xhttp;
             } else if ([4, 5].indexOf(co_sts) && "function" === typeof error) {
-              error(xhttp.status + ": " + status[1] + "\n" + lib._xhr_.call.url);
+              error(xhttp.status + ": " + status + ", url:" + lib._xhr_.call.url);
             } else {
-              throw Error("MyDOM " + lib._xhr_.call.method + " request to '" + lib._xhr_.call.url + "' returned code " + xhttp.status);
+              throw Error("MyDOM " + lib._xhr_.call.method + " request to '" + lib._xhr_.call.url + "' returned code " + xhttp.status + ": " + status);
             }
           };
           try {
@@ -405,65 +405,7 @@
     },
   };
   var _mdm_ = new MYD_();
-  _mdm_._xhr_.sts = {
-    200: "OK",
-    201: "Created",
-    202: "Accepted",
-    203: "Non-Authoritative Information (od HTTP/1.1)",
-    204: "No Content",
-    205: "Reset Content",
-    206: "Partial Content",
-    207: "Multi-Status (WebDAV) (RFC 4918)",
-    300: "Multiple Choices",
-    301: "Moved Permanently",
-    302: "Found",
-    303: "See Other (since HTTP/1.1)",
-    304: "Not Modified",
-    305: "Use Proxy (since HTTP/1.1)",
-    306: "Switch Proxy",
-    307: "Temporary Redirect (since HTTP/1.1)",
-    400: "Bad Request",
-    401: "Unauthorized",
-    402: "Payment Required",
-    403: "Forbidden",
-    404: "Not Found",
-    405: "Method Not Allowed",
-    406: "Not Acceptable",
-    407: "Proxy Authentication Required",
-    408: "Request Timeout",
-    409: "Conflict",
-    410: "Gone",
-    411: "Length Required",
-    412: "Precondition Failed",
-    413: "Request Entity Too Large",
-    414: "Request-URI Too Long",
-    415: "Unsupported Media Type",
-    416: "Requested Range Not Satisfiable",
-    417: "Expectation Failed",
-    418: "I'm a teapot",
-    422: "Unprocessable Entity (WebDAV) (RFC 4918)",
-    423: "Locked (WebDAV) (RFC 4918)",
-    424: "Failed Dependency (WebDAV) (RFC 4918)",
-    425: "Unordered Collection (RFC 3648)",
-    426: "Upgrade Required (RFC 7231)",
-    428: "Precondition Required (RFC 6585)",
-    429: "Too Many Requests (RFC 6585)",
-    431: "Request Header Fields Too Large (RFC 6585)",
-    449: "Retry With",
-    450: "Blocked by Windows Parental Controls",
-    451: "Unavailable For Legal Reasons",
-    499: "Client Closed Request",
-    500: "Internal Server Error",
-    501: "Not Implemented",
-    502: "Bad Gateway",
-    503: "Service Unavailable",
-    504: "Gateway Timeout",
-    505: "HTTP Version Not Supported",
-    506: "Variant Also Negotiates (RFC 2295)",
-    507: "Insufficient Storage (WebDAV) (RFC 4918)",
-    509: "Bandwidth Limit Exceeded (Apache bw/limited extension)",
-    510: "Not Extended (RFC 2774)"
-  };
+  _mdm_._xhr_.sts = JSON.parse(atob("eyIyMDAiOiJPSyIsIjIwMSI6IkNyZWF0ZWQiLCIyMDIiOiJBY2NlcHRlZCIsIjIwMyI6Ik5vbi1BdXRob3JpdGF0aXZlIEluZm9ybWF0aW9uIChvZCBIVFRQLzEuMSkiLCIyMDQiOiJObyBDb250ZW50IiwiMjA1IjoiUmVzZXQgQ29udGVudCIsIjIwNiI6IlBhcnRpYWwgQ29udGVudCIsIjIwNyI6Ik11bHRpLVN0YXR1cyAoV2ViREFWKSAoUkZDIDQ5MTgpIiwiMzAwIjoiTXVsdGlwbGUgQ2hvaWNlcyIsIjMwMSI6Ik1vdmVkIFBlcm1hbmVudGx5IiwiMzAyIjoiRm91bmQiLCIzMDMiOiJTZWUgT3RoZXIgKHNpbmNlIEhUVFAvMS4xKSIsIjMwNCI6Ik5vdCBNb2RpZmllZCIsIjMwNSI6IlVzZSBQcm94eSAoc2luY2UgSFRUUC8xLjEpIiwiMzA2IjoiU3dpdGNoIFByb3h5IiwiMzA3IjoiVGVtcG9yYXJ5IFJlZGlyZWN0IChzaW5jZSBIVFRQLzEuMSkiLCI0MDAiOiJCYWQgUmVxdWVzdCIsIjQwMSI6IlVuYXV0aG9yaXplZCIsIjQwMiI6IlBheW1lbnQgUmVxdWlyZWQiLCI0MDMiOiJGb3JiaWRkZW4iLCI0MDQiOiJOb3QgRm91bmQiLCI0MDUiOiJNZXRob2QgTm90IEFsbG93ZWQiLCI0MDYiOiJOb3QgQWNjZXB0YWJsZSIsIjQwNyI6IlByb3h5IEF1dGhlbnRpY2F0aW9uIFJlcXVpcmVkIiwiNDA4IjoiUmVxdWVzdCBUaW1lb3V0IiwiNDA5IjoiQ29uZmxpY3QiLCI0MTAiOiJHb25lIiwiNDExIjoiTGVuZ3RoIFJlcXVpcmVkIiwiNDEyIjoiUHJlY29uZGl0aW9uIEZhaWxlZCIsIjQxMyI6IlJlcXVlc3QgRW50aXR5IFRvbyBMYXJnZSIsIjQxNCI6IlJlcXVlc3QtVVJJIFRvbyBMb25nIiwiNDE1IjoiVW5zdXBwb3J0ZWQgTWVkaWEgVHlwZSIsIjQxNiI6IlJlcXVlc3RlZCBSYW5nZSBOb3QgU2F0aXNmaWFibGUiLCI0MTciOiJFeHBlY3RhdGlvbiBGYWlsZWQiLCI0MTgiOiJJJ20gYSB0ZWFwb3QiLCI0MjIiOiJVbnByb2Nlc3NhYmxlIEVudGl0eSAoV2ViREFWKSAoUkZDIDQ5MTgpIiwiNDIzIjoiTG9ja2VkIChXZWJEQVYpIChSRkMgNDkxOCkiLCI0MjQiOiJGYWlsZWQgRGVwZW5kZW5jeSAoV2ViREFWKSAoUkZDIDQ5MTgpIiwiNDI1IjoiVW5vcmRlcmVkIENvbGxlY3Rpb24gKFJGQyAzNjQ4KSIsIjQyNiI6IlVwZ3JhZGUgUmVxdWlyZWQgKFJGQyA3MjMxKSIsIjQyOCI6IlByZWNvbmRpdGlvbiBSZXF1aXJlZCAoUkZDIDY1ODUpIiwiNDI5IjoiVG9vIE1hbnkgUmVxdWVzdHMgKFJGQyA2NTg1KSIsIjQzMSI6IlJlcXVlc3QgSGVhZGVyIEZpZWxkcyBUb28gTGFyZ2UgKFJGQyA2NTg1KSIsIjQ0OSI6IlJldHJ5IFdpdGgiLCI0NTAiOiJCbG9ja2VkIGJ5IFdpbmRvd3MgUGFyZW50YWwgQ29udHJvbHMiLCI0NTEiOiJVbmF2YWlsYWJsZSBGb3IgTGVnYWwgUmVhc29ucyIsIjQ5OSI6IkNsaWVudCBDbG9zZWQgUmVxdWVzdCIsIjUwMCI6IkludGVybmFsIFNlcnZlciBFcnJvciIsIjUwMSI6Ik5vdCBJbXBsZW1lbnRlZCIsIjUwMiI6IkJhZCBHYXRld2F5IiwiNTAzIjoiU2VydmljZSBVbmF2YWlsYWJsZSIsIjUwNCI6IkdhdGV3YXkgVGltZW91dCIsIjUwNSI6IkhUVFAgVmVyc2lvbiBOb3QgU3VwcG9ydGVkIiwiNTA2IjoiVmFyaWFudCBBbHNvIE5lZ290aWF0ZXMgKFJGQyAyMjk1KSIsIjUwNyI6Ikluc3VmZmljaWVudCBTdG9yYWdlIChXZWJEQVYpIChSRkMgNDkxOCkiLCI1MDkiOiJCYW5kd2lkdGggTGltaXQgRXhjZWVkZWQgKEFwYWNoZSBidy9saW1pdGVkIGV4dGVuc2lvbikiLCI1MTAiOiJOb3QgRXh0ZW5kZWQgKFJGQyAyNzc0KSJ9"));
   _mdm_.helpers.elm = {
     css: {
       "Description": 'SET/GET css attribute with auto css prefixing (-webkit-,-moz-,-o-)',
